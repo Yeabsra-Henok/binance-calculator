5c771c3a89d7c1da195c988ff7c87c873a593b95
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2023-09-13 00:33:48 -0700

    commit - Added TP and SL

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const exitPriceInput = document.getElementById("exit-price");
   const quantityInput = document.getElementById("quantity");
   const leverageInput = document.getElementById("leverage");
+  const stopLossInput = document.getElementById("stop-loss");
+  const takeProfitInput = document.getElementById("take-profit");
   const liquidationPriceInput = document.getElementById("liquidation-price");
   const pnlInput = document.getElementById("pnl");
   const calculateButton = document.getElementById("calculate");
@@ -12,12 +14,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const exitPrice = parseFloat(exitPriceInput.value);
     const quantity = parseFloat(quantityInput.value);
     const leverage = parseFloat(leverageInput.value);
+    const stopLoss = parseFloat(stopLossInput.value);
+    const takeProfit = parseFloat(takeProfitInput.value);
 
     if (
       isNaN(entryPrice) ||
       isNaN(exitPrice) ||
       isNaN(quantity) ||
-      isNaN(leverage)
+      isNaN(leverage) ||
+      isNaN(stopLoss) ||
+      isNaN(takeProfit)
     ) {
       alert("Please enter valid numbers.");
       return;
@@ -25,8 +31,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const liquidationPrice = entryPrice - entryPrice / leverage;
     const pnl = (exitPrice - entryPrice) * quantity;
+    const stopLossPnl = (stopLoss - entryPrice) * quantity;
+    const takeProfitPnl = (takeProfit - entryPrice) * quantity;
 
     liquidationPriceInput.value = liquidationPrice.toFixed(2);
     pnlInput.value = pnl.toFixed(2);
+
+    // Display Stop Loss and Take Profit PNL
+    document.getElementById("stop-loss-pnl").textContent =
+      stopLossPnl.toFixed(2);
+    document.getElementById("take-profit-pnl").textContent =
+      takeProfitPnl.toFixed(2);
   });
 });
